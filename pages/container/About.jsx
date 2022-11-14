b5cebb5cfcd6656b5d0b676c1edab78945e64eed
Asraful <asrafulislamcodes@gmail.com>
2022-11-14 20:13:37 +0600

    added about

--- pages/container/About.jsx
+++ pages/container/About.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 
 import { AppWrap, MotionWrap } from '../wrapper';
+// import {urlFor,client} from "./"
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <motion.div className="flex flex-col items-center justify-center">
+      <h2 className="capitalize font-bold  text-2xl">
+        I am aware that <span className="text-[#313bac]">good business</span>{' '}
+        <br />
+        results from <span className="text-[#313bac]">good development</span>.
+      </h2>
+
+      <motion.div>
+        <motion.div></motion.div>
+      </motion.div>
+    </motion.div>
+  );
 };
 
 export default AppWrap(
